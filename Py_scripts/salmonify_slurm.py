7e5e65a0c495df74c433addef310515b7093a6e1
JWordsworth <jamesvwordsworth@gmail.com>
2019-11-19 14:27:01 +0000

    remove large file

--- Py_scripts/salmonify_slurm.py
+++ Py_scripts/salmonify_slurm.py
@@ -3,11 +3,11 @@ import argparse
 import subprocess
 
 
-def get_fastq_files_unpaired(directory):
+def get_fastq_files_single(directory):
     return glob.glob(os.path.join(directory, '*.1.fastq'))
 
 
-def get_fastq_files_paired(directory):
+def get_fastq_files_double(directory):
     ones = glob.glob(os.path.join(directory, '*.1.fastq'))
     twos = glob.glob(os.path.join(directory, '*.2.fastq'))
     return zip(ones, twos)
@@ -26,7 +26,7 @@ def build_job_scripts_single(fastq_files, index_dir, output_directory):
 
         temp_fastq_file = f'$TMPDIR/{accession}.fastq'
         temp_output = f'$TMPDIR/{accession}'
-        salmon = f'salmon quant -p 4 -l A -i {index_dir} --output {temp_output} --validateMappings -r {temp_fastq_file}'
+        salmon = f'salmon quant -p 4 -l A -i {index_dir} --output {temp_output} --validateMappings -1 {temp_fastq_file}'
 
         with open(job_script, 'w') as f:
             string = f'{shebang}\n{conda}\n{copy}\n{temp_fastq_file}\n{salmon}'
@@ -46,41 +46,9 @@ def run():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Salmonify a set of fastq files')
-    parser.add_argument('fastq_dir', help='The directory containing your fastq files')
-    parser.add_argument('out_dir', help='The directory to store your results')
+    parser.add_argument('directory', help='The directory containing your fastq files')
     parser.add_argument('--paired', help='When specified, stores True and you will run a paired analysis', action='store_true')
     args = parser.parse_args()
 
 
-    index_folder = '/mnt/storage/nobackup/njw262/RNAseq_Analysis2/transcripts_index'
-
-
-    # code for paired
-    if args.paired:
-        fastq_files = get_fastq_files_paired(args.fastq_dir)
-
-    # code for unpaired
-    else:
-        fastq_files = get_fastq_files_unpaired(args.fastq_dir)
-        build_job_scripts_single(fastq_files, index_folder, args.out_dir)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    index_folder = ''
