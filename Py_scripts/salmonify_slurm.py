61d47c2ce7e0bdc3e9b9dc5cdc971901bbac420b
JWordsworth <jamesvwordsworth@gmail.com>
2019-11-19 14:06:40 +0000

    changed salmon file again

--- Py_scripts/salmonify_slurm.py
+++ Py_scripts/salmonify_slurm.py
@@ -3,11 +3,11 @@ import argparse
 import subprocess
 
 
-def get_fastq_files_single(directory):
+def get_fastq_files_unpaired(directory):
     return glob.glob(os.path.join(directory, '*.1.fastq'))
 
 
-def get_fastq_files_double(directory):
+def get_fastq_files_paired(directory):
     ones = glob.glob(os.path.join(directory, '*.1.fastq'))
     twos = glob.glob(os.path.join(directory, '*.2.fastq'))
     return zip(ones, twos)
@@ -46,9 +46,41 @@ def run():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Salmonify a set of fastq files')
-    parser.add_argument('directory', help='The directory containing your fastq files')
+    parser.add_argument('fastq_dir', help='The directory containing your fastq files')
+    parser.add_argument('out_dir', help='The directory to store your results')
     parser.add_argument('--paired', help='When specified, stores True and you will run a paired analysis', action='store_true')
     args = parser.parse_args()
 
 
-    index_folder = ''
+    index_folder = '/mnt/storage/nobackup/njw262/RNAseq_Analysis2/transcripts_index'
+
+
+    # code for paired
+    if args.paired:
+        fastq_files = get_fastq_files_paired(args.fastq_dir)
+
+    # code for unpaired
+    else:
+        fastq_files = get_fastq_files_unpaired(args.fastq_dir)
+        build_job_scripts_single(fastq_files, index_folder, args.out_dir)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
